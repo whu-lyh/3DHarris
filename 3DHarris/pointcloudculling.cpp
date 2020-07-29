9ee81d6ae8e0f486f460d30ed30cbb7adc43b7ad
whu-lyh <2942563671@qq.com>
2020-07-29 19:23:33 +0800

    add multi las file batch process

--- 3DHarris/pointcloudculling.cpp
+++ 3DHarris/pointcloudculling.cpp
@@ -13,6 +13,8 @@
 #include "PointCloudIO.h"
 #include "PointType.h"
 
+#define DISTANCE 80
+
 //WUHAN DATASET
 bool loadPosTLin ( const std::string& filename, Utility::PoseVec& poses )
 {
@@ -43,70 +45,130 @@ bool loadPosTLin ( const std::string& filename, Utility::PoseVec& poses )
 	return true;
 }
 
+template <typename PointT>
+int selectTraject ( const boost::shared_ptr<pcl::PointCloud<PointT>> &cloud, std::vector<Utility::PoseVec>& vvec_poses )
+{
+	int traj_idx = -1;
+	{//binary divided to detect whether the trajecetory matches the point cloud, here the time should be monotonicial in MLS point cloud
+		int leftnumber = 0, rightnumber = cloud->points.size ();
+		while ( leftnumber < rightnumber )
+		{
+			const auto& pt = cloud->points [leftnumber];
+			double gps_time = pt.gps_time;
+
+			double min_gps_time = 0, max_gps_time = 0;
+
+			int posesize = vvec_poses.size ();
+			for ( int i = 0; i < posesize; ++i )
+			{
+				min_gps_time = vvec_poses [i][0].GPSTime;
+				max_gps_time = vvec_poses [i] [vvec_poses[i].size () - 1].GPSTime;
+				if ( gps_time >= min_gps_time && gps_time < max_gps_time )
+				{
+					traj_idx = i;
+					break;
+				}
+			}
+
+			if ( traj_idx != -1 ) break;
+			//still detect nothing, and here move to the 10th detect point and loop, max detect iteration is 10
+			leftnumber += rightnumber / 10;
+		}
+	}
+
+	//skip this function if this point cloud belongs to none of these trajectories
+	if ( traj_idx == -1 )
+	{
+		std::cout << "this point cloud doesn't belongs to the current trajectory!";
+		return -1;
+	}
+	return traj_idx;
+}
+
 int main ( int argc, char* argv [] )
 {
 	std::chrono::high_resolution_clock::time_point t1report = std::chrono::high_resolution_clock::now ();
 	//load trajectory
-	std::string filename = "D:/data/wuhangi/traj/2-east-iScan-Pos-1.lin";
-	std::string pointfilepath = "D:/data/wuhangi/pointcloud/2-east-iScan-Pcd-1_part0.las";
+	std::string filename = "D:/data/wuhangi/traj";
+	std::string pointfileoutpath = "D:/data/wuhangi/pointcloud-df";
+	std::string pointfilename = "D:/data/wuhangi/pointcloud";
 	
-	Utility::PoseVec vec_pose;
+	//读取全部轨迹，多个文件夹，并构建多个kd树
+	std::vector<std::string> trajfiles;
+	std::vector<pcl::KdTreeFLANN<pcl::PointXY>::Ptr> kdtree_vec ;
+	Utility::get_files ( filename, ".lin", trajfiles );
 
-	loadPosTLin ( filename, vec_pose );
-
-	//trajectory point cloud
-	boost::shared_ptr<pcl::PointCloud<pcl::PointXY>> traj_cloud ( new pcl::PointCloud<pcl::PointXY> () );
-	//for ( auto pose : vec_pose ) 
-	//could not use openmp
-	for(int i=0;i<vec_pose.size();++i)
+	std::vector<Utility::PoseVec> vvec_pose ( trajfiles.size () );
+	for (int trajid=0;trajid<trajfiles.size();++trajid)
 	{
-		pcl::PointXY pt;
-		pt.x = vec_pose[i].x;
-		pt.y = vec_pose[i].y;
-		traj_cloud->push_back ( pt );
+		loadPosTLin ( trajfiles[trajid], vvec_pose[trajid] );
+
+		//trajectory point cloud
+		boost::shared_ptr<pcl::PointCloud<pcl::PointXY>> traj_cloud ( new pcl::PointCloud<pcl::PointXY> () );
+		//could not use openmp
+		for ( int i = 0; i < vvec_pose [trajid].size (); ++i )
+		{
+			pcl::PointXY pt;
+			pt.x = vvec_pose [trajid] [i].x;
+			pt.y = vvec_pose [trajid] [i].y;
+			traj_cloud->push_back ( pt );
+		}
+
+		pcl::KdTreeFLANN<pcl::PointXY>::Ptr kdtree = boost::make_shared<pcl::KdTreeFLANN<pcl::PointXY>> ();
+		kdtree->setInputCloud ( traj_cloud );
+		kdtree_vec.push_back ( kdtree );
 	}
 
-	pcl::KdTreeFLANN<pcl::PointXY>::Ptr kdtree = boost::make_shared<pcl::KdTreeFLANN<pcl::PointXY>> ();
-	kdtree->setInputCloud ( traj_cloud );
+	//判断点云和轨迹是不是重合的，将每一份点云对应的kd树拿出来开始剔除。
+	std::vector<std::string> pointfiles;
+	Utility::get_files ( pointfilename, ".las", pointfiles );
 
-	pcl::PointCloud<PointXYZINTF>::Ptr input_cloud = boost::make_shared<pcl::PointCloud<PointXYZINTF>> ();
-	pcl::PointCloud<PointXYZINTF>::Ptr output_cloud = boost::make_shared<pcl::PointCloud<PointXYZINTF>> ();
-	Utility::Offset las_offset;
-	PointIO::loadSingleLAS<PointXYZINTF> ( pointfilepath, input_cloud, las_offset );
-
-	std::vector<int> pointIdxRadiusSearch;
-	std::vector<float> pointRadiusSquaredDistance;
-
-	for ( auto pt : input_cloud->points ) 
+	for ( auto pointfile : pointfiles ) 
 	{
-		pcl::PointXY search_pt;
-		search_pt.x = pt.x + las_offset.x;
-		search_pt.y = pt.y + las_offset.y;
+		pcl::PointCloud<PointXYZINTF>::Ptr input_cloud = boost::make_shared<pcl::PointCloud<PointXYZINTF>> ();
+		pcl::PointCloud<PointXYZINTF>::Ptr output_cloud = boost::make_shared<pcl::PointCloud<PointXYZINTF>> ();
+		Utility::Offset las_offset;
+		PointIO::loadSingleLAS<PointXYZINTF> ( pointfile, input_cloud, las_offset );
 
-		if ( kdtree->nearestKSearch ( search_pt, 1, pointIdxRadiusSearch, pointRadiusSquaredDistance ) )
+		int trajid = selectTraject ( input_cloud, vvec_pose );
+
+		std::vector<int> pointIdxRadiusSearch;
+		std::vector<float> pointRadiusSquaredDistance;
+
+		for ( auto pt : input_cloud->points )
 		{
-			if ( pointIdxRadiusSearch.empty () ) 
-			{
-				std::cout << "Point index extracted failed";
-				continue;
-			}
+			pcl::PointXY search_pt;
+			search_pt.x = pt.x + las_offset.x;
+			search_pt.y = pt.y + las_offset.y;
 
-			if ( pointRadiusSquaredDistance.empty () ) 
+			if ( kdtree_vec[trajid]->nearestKSearch ( search_pt, 1, pointIdxRadiusSearch, pointRadiusSquaredDistance ) )
 			{
-				std::cout << "Distance hasn't been parsed correctly";
-				continue;
-			}
+				if ( pointIdxRadiusSearch.empty () )
+				{
+					std::cout << "Point index extracted failed";
+					continue;
+				}
 
-			if ( std::sqrt ( pointRadiusSquaredDistance [0] ) < 100 ) 
-			{
-				output_cloud->push_back ( pt );
+				if ( pointRadiusSquaredDistance.empty () )
+				{
+					std::cout << "Distance hasn't been parsed correctly";
+					continue;
+				}
+
+				if ( std::sqrt ( pointRadiusSquaredDistance [0] ) < DISTANCE )
+				{
+					output_cloud->push_back ( pt );
+				}
 			}
 		}
+
+		std::string outfilepath = pointfileoutpath + "/" + Utility::get_name ( pointfile );
+		std::cout << "origin pts:\t" << input_cloud->size () << " left pts: \t" << output_cloud->size ();
+		
+		//release memory and save
+		input_cloud->swap ( pcl::PointCloud<PointXYZINTF > () );
+		PointIO::saveLAS<PointXYZINTF> ( outfilepath, output_cloud, las_offset );
 	}
-	
-	std::string outfilepath = Utility::get_parent ( pointfilepath ) + "/" + Utility::get_name_without_ext ( pointfilepath ) + "_df.las";
-	std::cout << "origin pts:\t" << input_cloud->size () << " left pts: \t" << output_cloud->size ();
-	PointIO::saveLAS<PointXYZINTF> ( outfilepath, output_cloud, las_offset );
 
 	std::chrono::high_resolution_clock::time_point t2treport = std::chrono::high_resolution_clock::now ();
 	std::chrono::duration<double> t12report = std::chrono::duration_cast<std::chrono::duration<double>>( t2treport - t1report );
